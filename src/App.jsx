7cd204f5c9f8a0a7d862867c7605172ccd4a920d
Madhav Jagad <madhavjagad@gmail.com>
2026-01-21 16:55:12 +0530

    Added the art section with style and GSAP animation

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Cocktails from "./Components/Cocktails";
 import About from "./Components/About";
+import Art from "./Components/Art";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -16,6 +17,7 @@ const App = () => {
         <Hero />
         <Cocktails />
         <About />
+        <Art />
       </main>
     </>
   );
